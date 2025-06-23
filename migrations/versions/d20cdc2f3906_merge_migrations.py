8deb7ece16959b66a81af09cac575b13d64f4544
Maria Carolina Kopp <maria.carolska@gmail.com>
2025-06-23 14:47:01 -0300

    sistema funcionando

---
+++ migrations/versions/d20cdc2f3906_merge_migrations.py
@@ -0,0 +1,24 @@
+"""merge_migrations
+
+Revision ID: d20cdc2f3906
+Revises: 
+Create Date: 2025-06-23 13:48:06.773790
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd20cdc2f3906'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
